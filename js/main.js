c8654c989ae92274bd74d314b2f826fabc332ac9
Тимур <dox1gun@gmail.com>
2019-06-12 19:50:04 +0300

    Сделал функцию для создания изображений

--- js/main.js
+++ js/main.js
@@ -11,23 +11,17 @@ var MAP_WIDTH = map.clientWidth;
 var MAP_HEIGHT_MIN = 130;
 var MAP_HEIGHT_MAX = 630;
 
-var avatars = [
-  'img/avatars/user01.png',
-  'img/avatars/user02.png',
-  'img/avatars/user03.png',
-  'img/avatars/user04.png',
-  'img/avatars/user05.png',
-  'img/avatars/user06.png',
-  'img/avatars/user07.png',
-  'img/avatars/user08.png'
-];
-
 var offerType = [
   'palace',
   'flat',
   'bungalo',
   'house'
 ];
+
+var getAvatar = function (quantity) {
+  var avatar = 'img/avatars/user0' + quantity + '.png';
+  return avatar;
+};
 
 var getRandomValue = function (inputValue) {
   var outputValue = Math.round(Math.random() * inputValue);
@@ -39,23 +33,25 @@ var getRandomHeight = function (min, max) {
   return outputValue;
 };
 
-var getAnnouncement = function (avatar, offer, x, yMin, yMax) {
+var getAnnouncement = function (offer, x, yMin, yMax) {
   var NUMBER_OF_ANNOUNCEMENTS = 8;
-  var arrayOfAnnouncements = [];
+  var announcements = [];
 
   for (var i = 0; i < NUMBER_OF_ANNOUNCEMENTS; i++) {
     var announcement = {};
-    announcement.author = avatar[i];
+    announcement.author = getAvatar(i);
     announcement.offer = offer[getRandomValue(offer.length - 1)];
     announcement.locationX = getRandomValue(x);
     announcement.locationY = getRandomHeight(yMin, yMax);
-    arrayOfAnnouncements.push(announcement);
+    announcements.push(announcement);
   }
-  return arrayOfAnnouncements;
+
+  return announcements;
 };
 
 var createTemplate = function (array) {
   var pinsTemplates = document.createDocumentFragment();
+
   for (var i = 0; i < array.length; i++) {
     var fragment = pinTemplate.cloneNode(true);
     fragment.style.left = (array[i].locationX - (fragment.clientWidth / 2)) + 'px';
@@ -63,7 +59,8 @@ var createTemplate = function (array) {
     fragment.children[0].src = array[i].author;
     pinsTemplates.appendChild(fragment);
   }
+
   mapPins.appendChild(pinsTemplates);
 };
 
-createTemplate(getAnnouncement(avatars, offerType, MAP_WIDTH, MAP_HEIGHT_MIN, MAP_HEIGHT_MAX));
+createTemplate(getAnnouncement(offerType, MAP_WIDTH, MAP_HEIGHT_MIN, MAP_HEIGHT_MAX));
