9b3ccf2aeb80837f032b194bce087fd2b890e621
Тимур <dox1gun@gmail.com>
2019-07-12 22:11:26 +0300

    правки

--- js/data.js
+++ js/data.js
@@ -2,6 +2,7 @@
 
 (function () {
   var GET_ADDRESS = 'https://js.dump.academy/keksobooking/data';
+  var POST_ADDRESS = 'https://js.dump.academy/keksobooking';
   var SUCCESS_CODE = 200;
   window.data = {
     load: function (onLoad, onError) {
@@ -18,6 +19,21 @@
 
       window.xhr.open('GET', GET_ADDRESS);
       window.xhr.send();
+    },
+    save: function (data, onLoad, onError) {
+      window.xhr = new XMLHttpRequest();
+      window.xhr.responseType = 'json';
+
+      window.xhr.addEventListener('load', function () {
+        if (window.xhr.status === SUCCESS_CODE) {
+          onLoad();
+        } else {
+          onError();
+        }
+      });
+
+      window.xhr.open('POST', POST_ADDRESS);
+      window.xhr.send(data);
     }
   };
 })();
